f5fff3e847117c5a82bf05923224962da548947d
Christian Ortiz <christianortizvargas@gmail.com>
2023-11-14 00:21:27 -0300

    Agregue formulario para tipos, evento y sus controladores

---
+++ Controllers/EventoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebValdiviaDojo.Controllers
+{
+    public class EventoController : Controller
+    {
+        // GET: Evento
+        public ActionResult Evento()
+        {
+            return View();
+        }
+    }
+}
