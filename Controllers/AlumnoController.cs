de4c83341608b4f9f40630fb3ff19219601f0d24
Mitimity <mar.riosv@duocuc.cl>
2023-12-07 15:20:55 -0300

    Cambios en las solicitudes

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -24,8 +24,39 @@ namespace WebValdiviaDojo.Controllers
             return View();
         }
 
+        public ActionResult admSolicitudAlumno(string v_rut)
+        {
+            if (v_rut == null)
+            {
+                ViewBag.Mensaje = "Debe iniciar sesión";
+            }
+            List<tipoSolicitud> ob = ListarTipoSolicitud();
+            List<solicitud> Solicitud = ListarSolicitud(v_rut);
+            ViewBag.tpSolicitud = ob;
+            ViewBag.sol = Solicitud;
 
+            return View();
+        }
 
+        //LISTAR
+        public List<solicitud> ListarSolicitud(string p_rut)
+        {
+            WS_DojoClient cliente = new WS_DojoClient();
+
+            try
+            {
+                return cliente.ListaSolicitud(null, null, p_rut).ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al llamar al servicio web: " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                cliente.Close();
+            }
+        }
         public List<tipoSolicitud> ListarTipoSolicitud()
         {
             WS_DojoClient cliente = new WS_DojoClient();
@@ -44,6 +75,8 @@ namespace WebValdiviaDojo.Controllers
                 cliente.Close();
             }
         }
+
+        //FUNCIONES
 
         [HttpPost]
         public ActionResult EnvioSolicitud(string p_sol, int p_tipo, int p_rut)
@@ -66,8 +99,63 @@ namespace WebValdiviaDojo.Controllers
             }
 
             // Redirigir a la página de inicio
-            return RedirectToAction("EnvioSolicitud", new { v_rut = p_rut });
+            return RedirectToAction("admSolicitudAlumno", new { v_rut = p_rut });
         }
 
+
+        [HttpPost]
+        public ActionResult ModificarSolicitud(int p_id, string p_sol, string p_res, int p_tipo_sol, int p_estado, int p_rut)
+        {
+            WS_DojoClient cliente = new WS_DojoClient();
+
+            try
+            {
+                cliente.ModSolicitud(p_id, p_sol, p_res, p_tipo_sol, p_estado, p_rut);
+                ViewBag.Mensaje = "Solicitud Modificada";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al llamar al servicio web: " + ex.Message);
+                return RedirectToAction("admSolicitudAlumno", new { v_rut = p_rut });
+            }
+            finally
+            {
+                cliente.Close();
+            }
+
+            // Redirigir a la página de inicio
+            return RedirectToAction("admSolicitudAlumno", new { v_rut = p_rut });
+        }
+        [HttpPost]
+        public ActionResult ElSolicitud(int p_id,int v_rut)
+        {
+            WS_DojoClient cliente = new WS_DojoClient();
+
+            try
+            {
+                cliente.EliSolicitud(p_id);
+                ViewBag.Mensaje = "Solicitud eliminada";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al llamar al servicio web: " + ex.Message);
+                return RedirectToAction("admSolicitudAlumno", new { v_rut = v_rut });
+            }
+            finally
+            {
+                cliente.Close();
+            }
+
+            // Redirigir a la página de inicio
+            return RedirectToAction("admSolicitudAlumno", new { v_rut = v_rut  });
+        }
     }
 }
+
+
+
+
+
+
+
+
